cfb627c5bc28a1622a5a8fd894534216e59cb9f9
Jeni <frei_jennifer_charle@student.smc.edu>
2026-07-06 20:32:36 -0700

    mod3

---
+++ module3/ContactForm.php
@@ -0,0 +1,29 @@
+// Jennifer Frei
+//CS85 Sec 1234
+//Module3 Assignment 3B
+// GitHub Repo URL: https://github.com/Jentare/cs85-module3b-createform
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Contact Form   </title>
+    </head>
+    <body>
+        <form action="" method="POST">
+            <label for="FullName">Full Name:</label><br>
+            <input type="text" id="FullName" name="FullName" required><br><br>
+            
+            <label for="Email">Email Address:</label><br>
+            <input type="email" id="Email" name="Email" required><br><br>
+            
+            <label for="Topic">Topic:</label><br>
+            <input type="text" id="Topic" name="Topic" required><br><br>
+            
+            <label for="Message">Message:</label><br>
+            <input type="text" id="Message" name="Message" required><br><br>
+            
+            <input type="submit" name="submit" value="Submit Message"><br><br>
+    </form>
+    </body>
+</html>
